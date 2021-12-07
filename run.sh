7d2d56e8c72c7cb447c0622192a798924c340174
muzhial <kantchenjn@163.com>
2021-12-07 02:25:44 +0000

    mull

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-# para=("001" "002" "003" "006" "009" "010", "011", "012", "013")
-para=("010", "011")
+para=("001" "002" "003" "006" "009" "010", "011", "012", "013")
+
 for i in ${para[*]}; do
 python car_tracking_server.py \
     --VIDEO_PATH  /dataset/mz/code/yoloxServer-vehicle/record1/$i.mp4 \
